f34b04d0d31272e80e131849acd194acaa2cf05b
Chieh-Chun Lin <sukusukustudio@protonmail.com>
2024-06-02 07:40:41 +0800

    revise funcDB.js to throw error directly

--- database/funcDB.js
+++ database/funcDB.js
@@ -6,14 +6,9 @@ async function newUrl(conn, longs, shorts) {
     `,
     [longs, shorts]
   );
-  try {
-    // console.log("insert rows %j", rows);
-    // {"fieldCount":0,"affectedRows":1,"insertId":3,"info":"","serverStatus":2,"warningStatus":0,"changedRows":0}
-    return rows.insertId;
-  } catch (err) {
-    console.log(err);
-    return undefined; //or throw error directly?
-  }
+  // console.log("insert rows %j", rows);
+  // {"fieldCount":0,"affectedRows":1,"insertId":3,"info":"","serverStatus":2,"warningStatus":0,"changedRows":0}
+  return rows.insertId;
 }
 async function updateClicks(conn, shorts, clicks) {
   const [rows] = await conn.query(
@@ -24,14 +19,9 @@ async function updateClicks(conn, shorts, clicks) {
     `,
     [clicks, shorts]
   );
-  try {
-    //console.log("update rows %j", rows);
-    //{"fieldCount":0,"affectedRows":1,"insertId":0,"info":"Rows matched: 1  Changed: 1  Warnings: 0","serverStatus":2,"warningStatus":0,"changedRows":1}
-    return rows.changedRows;
-  } catch (err) {
-    console.log(err);
-    return undefined; //or throw error directly?
-  }
+  //console.log("update rows %j", rows);
+  //{"fieldCount":0,"affectedRows":1,"insertId":0,"info":"Rows matched: 1  Changed: 1  Warnings: 0","serverStatus":2,"warningStatus":0,"changedRows":1}
+  return rows.changedRows;
 }
 async function getUrl(conn, start, pageSize, search = "") {
   let condition = search == "" ? "" : ` WHERE longs LIKE '%${search}%'`;
@@ -44,14 +34,9 @@ async function getUrl(conn, start, pageSize, search = "") {
     `,
     [start, pageSize]
   );
-  try {
-    // console.log("select rows %j", rows);
-    //[{"id":4,"longs":"https://blog.medium.com/bnp-editors-9c0a6f5a133a","shorts":"https://www.example.com/2","clicks":0}]
-    return rows;
-  } catch (err) {
-    console.log(err);
-    return undefined; //or throw error directly?
-  }
+  // console.log("select rows %j", rows);
+  //[{"id":4,"longs":"https://blog.medium.com/bnp-editors-9c0a6f5a133a","shorts":"https://www.example.com/2","clicks":0}]
+  return rows;
 }
 async function deleteUrl(conn, shorts) {
   const [rows] = await conn.query(
@@ -61,14 +46,9 @@ async function deleteUrl(conn, shorts) {
     `,
     [shorts]
   );
-  try {
-    // console.log("delete rows %j", rows);
-    // {"fieldCount":0,"affectedRows":1,"insertId":0,"info":"","serverStatus":2,"warningStatus":0,"changedRows":0}
-    return rows.insertId;
-  } catch (err) {
-    console.log(err);
-    return undefined; //or throw error directly?
-  }
+  // console.log("delete rows %j", rows);
+  // {"fieldCount":0,"affectedRows":1,"insertId":0,"info":"","serverStatus":2,"warningStatus":0,"changedRows":0}
+  return rows.insertId;
 }
 
 export default {
